f7251da7fc162ece3c9e4d7981eba61e4ffe8879
iyz <iy.usa@outlook.com>
2024-09-22 19:03:12 -0400

    add logos to header and home page, styles

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+import BrandLogo from "@/../public/logos/horizontal-white-blue-logo.png";
 
 export default function Header() {
   return (
-    <header className="h-14 dark-purple-border text-white flex justify-between items-center px-12 border-b-2 border-black">
-      <h1>File Portal</h1>
+    <header className="h-16 dark-purple-border text-white flex justify-between items-center px-12 border-b-2 border-black">
+      <Image src={BrandLogo} alt="Brand Logo" width={250} height={250} />
       <div className="flex gap-8">
         <Link href="/">Login</Link>
         <Link href="/">About</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,43 @@ import Image from "next/image";
 import PortalImage from "@/../public/portal.png";
 import ChestClosed from "@/../public/chest-closed.png";
 import ChestOpened from "@/../public/chest-opened.png";
+import BrandLogo from "@/../public/logos/vertical-white-blue-logo.png";
 
 export default function Home() {
   return (
     <div className="dark-purple-bg flex flex-col justify-center items-center min-h-screen">
-      {/* <main className="flex gap-8 row-start-2 justify-center items-center sm:items-start dark-purple-bg"> */}
-      {/* <Image src={ChestClosed} alt="Chest Closed" width={200} height={200} /> */}
+      <Image src={BrandLogo} alt="Brand Logo" width={250} height={250} />
       <div className="w-[100%] flex justify-center items-center">
-        <Image src={ChestOpened} alt="Chest Opened" width={300} height={300} />
-        <Image src={PortalImage} alt="Portal" width={300} height={300} />
-      </div>
+        <div>
+          <Image
+            src={ChestClosed}
+            alt="Chest Closed"
+            width={250}
+            height={250}
+          />
+          <button className="bg-white text-black px-4 py-2 rounded-md">
+            Store Files
+          </button>
+        </div>
+        <div>
+          <Image
+            src={ChestOpened}
+            alt="Chest Opened"
+            width={300}
+            height={300}
+          />
+          <button className="bg-white text-black px-4 py-2 rounded-md">
+            Open Files
+          </button>
+        </div>
 
-      {/* </main> */}
+        <div>
+          <Image src={PortalImage} alt="Portal" width={300} height={300} />
+          <button className="bg-white text-black px-4 py-2 rounded-md">
+            Open Portal
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
