72b57b5f8689047c541acf7dd4e9631938e454e6
iyz <iy.usa@outlook.com>
2024-09-22 14:04:43 -0400

    add starting code, add readme file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/iyilmaz24/FilePortal/blob/main/README.md"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -63,11 +63,12 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          How to use
         </a>
+
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/iyilmaz24/FilePortal"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -78,22 +79,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          GitHub Repository
         </a>
       </footer>
     </div>
