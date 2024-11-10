a00729947eb30d9b2c9a1b5d9555cb7d9bb375b6
iyz <iy.usa@outlook.com>
2024-11-10 17:01:24 -0500

    create working HTTP server
    - issue #7 from project issue tracker

--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	httpPort := 8080
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "You requested %s\n", r.URL.Path)
+		if err != nil {
+			log.Printf("Error Returning Response: %v\n", err.Error())
+			http.Error(w, fmt.Sprintf("Internal Server Error: %v", err.Error()), http.StatusInternalServerError)
+		}
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
 }
